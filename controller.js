4725fe3ec1734ed5483ad673b796326aca9c2e35
Hapid <abiyass25@gmail.com>
2023-03-21 11:22:40 +0700

    membuat route

--- controller.js
+++ controller.js
@@ -6,3 +6,15 @@ var connection = require("./koneksi");
 exports.index = function (req, res) {
   response.ok("Aplikasi REST API berjalan !", res);
 };
+
+// menampilkan semua data blog
+
+exports.tampildatablogall = function (req, res) {
+  connection.query("SELECT * FROM blog_tbl", function (error, rows, fields) {
+    if (error) {
+      connection.log(error);
+    } else {
+      response.ok(rows, res);
+    }
+  });
+};
