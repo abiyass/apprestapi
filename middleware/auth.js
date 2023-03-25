770f1eb7eeb18f1b9db3b8fd05363717b487ce89
Hapid <abiyass25@gmail.com>
2023-03-25 10:18:23 +0700

    login user berhasil

--- middleware/auth.js
+++ middleware/auth.js
@@ -6,7 +6,7 @@ var jwt = require("jsonwebtoken");
 var config = require("../config/secret");
 var ip = require("ip");
 
-exports.regitrasi = function (req, res) {
+exports.registrasi = function (req, res) {
   var post = {
     nama_user: req.body.nama_user,
     pass_user: md5(req.body.pass_user),
@@ -37,6 +37,62 @@ exports.regitrasi = function (req, res) {
         });
       } else {
         response.ok("Nama User sudah terdaftar !", res);
+      }
+    }
+  });
+};
+
+// controller untuk login
+
+exports.login = function (req, res) {
+  var post = {
+    pass_user: req.body.pass_user,
+    nama_user: req.body.nama_user,
+  };
+
+  var query = "SELECT * FROM ?? WHERE ??=? AND ??=?";
+  var table = [
+    "user_tbl",
+    "pass_user",
+    md5(post.pass_user),
+    "nama_user",
+    post.nama_user,
+  ];
+
+  query = mysql.format(query, table);
+  connection.query(query, function (error, rows) {
+    if (error) {
+      console.log(error);
+    } else {
+      if (rows.length == 1) {
+        var token = jwt.sign({ rows }, config.secret, { expiresIn: 1440 });
+        id_user = rows[0].id_user;
+        var data = {
+          id_user: id_user,
+          token_akses: token,
+          ip_address: ip.address(),
+        };
+
+        var query = "INSERT INTO ?? SET ?";
+        var table = ["akses_token"];
+        query = mysql.format(query, table);
+        connection.query(query, data, function (error, rows) {
+          if (error) {
+            console.log(error);
+          } else {
+            res.json({
+              success: true,
+              message: "Token JWT tergenerate",
+              token: token,
+              currUser: data.id_user,
+            });
+          }
+        });
+      } else {
+        res.json({
+          error: true,
+          message: "Nama User atau Password Salah ",
+        });
       }
     }
   });
